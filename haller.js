17bcaa46de38d579f7101044320db872290b39a6
Murat Çorlu <muratcorlu@gmail.com>
2013-01-07 22:04:11 +0200

    refactor

--- haller.js
+++ haller.js
@@ -4,60 +4,58 @@
 * sercaneraslan.com
 *
 * Kullanımı örnekleri:
-* console.log(Haller.init("sercan","de"));
-* console.log(Haller.init("Murat","iyelik"));
-* console.log(Haller.init("osman","i"));
+* console.log(Hal.ler("sercan","de"));
+* console.log(Hal.ler("Murat","iyelik"));
+* console.log(Hal.ler("osman","i"));
 */
-var Haller = (function( undefined ){
+var Hal = (function( undefined ){
     var sesliHarfler = 'aıeiouöü',
         sertUnsuzler = 'fstkçşhp',
         haller = ['iyelik','i','e','de','den'];
 
     return {
-        init: function(isim, hal) {
+        ler: function(isim, hal) {
             var harfler = isim.split(''),
                 sonHarf = harfler.pop(),
-                sesliIleMiBitiyor = sesliHarfler.indexOf( sonHarf ) > -1,
-                sertUnsuzleMiBitiyor = !sesliIleMiBitiyor && sertUnsuzler.indexOf( sonHarf ) > -1,
-                sonSesli = (function(){
-                    while ( sesliHarfler.indexOf(sonHarf) === -1) {
-                        sonHarf = harfler.pop();
+                harf,
+                sesliler=[],
+                ek,
+                code,
+                sonSesli;
 
-                        if (sonHarf == undefined) break; // Isimde hic sesli harf yok!
+            while ( (harf = harfler.pop()) !== undefined ) {
+                (code = sesliHarfler.indexOf(harf)) > -1 && sesliler.push(code);
+            }
 
-                    }
-
-                    return sesliHarfler.indexOf(sonHarf);
-                })(),
-                ek;
+            sonSesli = sesliler[0];
 
             // Ekin sesli harfine karar verelim
-            if (hal === haller[0] || hal === haller[1]) {
-                ek = (sonSesli === 0 || sonSesli === 1) && 'ı' || // Son sesli harf a veya ı ise
-                     (sonSesli === 2 || sonSesli === 3) && 'i' || // Son sesli harf e veya i ise
-                     (sonSesli === 4 || sonSesli === 5) && 'u' || // Son sesli harf o veya u ise
+            if (hal == haller[0] || hal == haller[1]) {
+                ek = (sonSesli == 0 || sonSesli == 1) && 'ı' || // Son sesli harf a veya ı ise
+                     (sonSesli == 2 || sonSesli == 3) && 'i' || // Son sesli harf e veya i ise
+                     (sonSesli == 4 || sonSesli == 5) && 'u' || // Son sesli harf o veya u ise
                      'ü';   // Son sesli harf ö veya ü ise
             } else {
-                ek = (sonSesli === 0 || sonSesli === 1 ||
-                        sonSesli === 4 || sonSesli === 5) && 'a' || // Son sesli harf a, ı, o veya u ise
+                ek = (sonSesli == 0 || sonSesli == 1 ||
+                        sonSesli == 4 || sonSesli == 5) && 'a' || // Son sesli harf a, ı, o veya u ise
                     'e';  // Son sesli harf e, i, ö veya ü ise
             }
 
             // Ön ekleri belirleyelim (kaynaştırma harfi)
-            if (sesliIleMiBitiyor) {
-                if (hal === haller[0]) { // iyelik hali ise
+            if ( sesliHarfler.indexOf( sonHarf ) > -1 ) { // Sesli ile mi bitiyor
+                if (hal == haller[0]) { // iyelik hali ise
                     ek = 'n' + ek
                 }
-                if (hal === haller[1] || hal === haller[2]) { // i veya e hali ise
+                if (hal == haller[1] || hal == haller[2]) { // i veya e hali ise
                     ek = 'y' + ek
                 }
             }
-            if (hal === haller[3]  || hal === haller[4]) { // de veya den hali ise
-                ek = (sertUnsuzleMiBitiyor ? 't' : 'd') + ek;
+            if (hal == haller[3]  || hal == haller[4]) { // de veya den hali ise
+                ek = (sertUnsuzler.indexOf( sonHarf ) > -1 ? 't' : 'd') + ek;
             }
 
             // Son ekleri belirleyelim
-            if (hal === haller[0] || hal === haller[4]) { // iyelik veya den hali ise
+            if (hal == haller[0] || hal == haller[4]) { // iyelik veya den hali ise
                 ek += 'n'
             }
 
